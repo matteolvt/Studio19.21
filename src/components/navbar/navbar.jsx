4afe7c7ca0a0bc99b86f87b6026502be3213c94f
matteolvt <146728526+matteolvt@users.noreply.github.com>
2025-12-23 19:24:21 +0100

    fix: correction des chemins assets pour le build

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./navbar.css";
-import logo from "../../assets/logo.svg";
+import logo from "../../assets/Logo.svg";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
