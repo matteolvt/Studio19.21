3fd51ad36cdbe3df2bf951a0117564684799e15d
Julliat Tom <tomjulliat07@gmail.com>
2025-12-24 17:18:38 +0100

    nettoyage

--- src/components/AppointmentProvider/AppointmentProvider.jsx
+++ src/components/AppointmentProvider/AppointmentProvider.jsx
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { db } from "../../services/firebase";
 import { collection, addDoc, getDocs, query, where } from "firebase/firestore";
 import "./AppointmentProvider.css";
