38130888b7ac57886e553067aadd74dc6794cafe
matteolvt <146728526+matteolvt@users.noreply.github.com>
2025-12-24 18:22:45 +0100

    feat: add maintenance page for public launch

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,7 @@
-import React from "react";
-import Snowfall from "react-snowfall";
-import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/homepage/homepage";
-import Projets from "./pages/projets/projets";
+import Maintenance from "./pages//maintenance/Maintenance.jsx";
 
 function App() {
-  return (
-    <>
-      <Snowfall
-        color="#DDE7EF"
-        snowflakeCount={200}
-        style={{
-          position: "fixed",
-          width: "100vw",
-          height: "100vh",
-          zIndex: 9999,
-          pointerEvents: "none",
-        }}
-      />
-
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/projets" element={<Projets />} />
-      </Routes>
-    </>
-  );
+  return <Maintenance />;
 }
 
 export default App;
