88c9469dd7b4bc6e00fd749821a8456f0d2eb0eb
matteolvt <146728526+matteolvt@users.noreply.github.com>
2025-12-27 04:44:24 +0100

    fix:probleme vercel

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -3,9 +3,9 @@ import AboutHero from "../../components/AboutHero/AboutHero";
 import StorySection from "../../components/StorySection/StorySection";
 import ValuesSection from "../../components/ValuesSection/ValuesSection";
 import TeamSection from "../../components/TeamSection/TeamSection";
-import Navbar from "../../components/navbar/Navbar";
+import Navbar from "../../components/navbar/navbar";
 import StatsSection from "../../components/StatsSection/StatsSection";
-import Footer from "../../components/footer/Footer";
+import Footer from "../../components/footer/footer";
 import "./about.css";
 
 const About = () => {
